1151a9fef5dc36a29e204a95cf3facaf1bb0a9db
Darpan <darpan.kapadia@gmail.com>
2023-08-28 20:19:40 +1200

    Created Navigation

--- app/src/main/java/com/dk/boruto/MainActivity.kt
+++ app/src/main/java/com/dk/boruto/MainActivity.kt
@@ -3,6 +3,9 @@ package com.dk.boruto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dk.boruto.navigation.SetupNavGraph
 import com.dk.boruto.ui.theme.BorutoTheme
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
@@ -11,7 +14,10 @@ import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         AppCenter.start(
             application, "df7e485e-ab33-46ec-95e8-abac6bcba7b4",
@@ -21,6 +27,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             BorutoTheme {
 
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+                
             }
         }
     }
