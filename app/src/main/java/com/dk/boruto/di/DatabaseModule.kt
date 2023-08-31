882af1373c5f28d6684607d35aafaa7de3f03b80
Darpan <darpan.kapadia@gmail.com>
2023-08-31 18:59:02 +1200

    Create Database di Module

---
+++ app/src/main/java/com/dk/boruto/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.dk.boruto.di
+
+import android.content.Context
+import androidx.room.Room
+import com.dk.boruto.data.local.BorutoDatabase
+import com.dk.boruto.util.Constants.BORUTO_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        BorutoDatabase::class.java,
+        BORUTO_DATABASE
+    ).build()
+
+}
