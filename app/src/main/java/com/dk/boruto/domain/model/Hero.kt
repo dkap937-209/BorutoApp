a0dbe181e521e6ce9efb737fbe46c81a697de368
Darpan <darpan.kapadia@gmail.com>
2023-09-10 09:25:16 +1200

    Created ApiResponse and BorutoAPI interface

--- app/src/main/java/com/dk/boruto/domain/model/Hero.kt
+++ app/src/main/java/com/dk/boruto/domain/model/Hero.kt
@@ -3,7 +3,9 @@ package com.dk.boruto.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.dk.boruto.util.Constants.HERO_DATABASE_TABLE
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = HERO_DATABASE_TABLE)
 data class Hero(
     @PrimaryKey(autoGenerate = false)
